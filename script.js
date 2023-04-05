c23a67b5bab7c8cf6b4f9d42de17241b4b62e5dd
Jovi <Rondo2014@outlook.com>
2023-04-05 11:22:21 -0700

    Checkerboard with random colors and better background

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const betterBackground = (document.body.style.backgroundColor = "#1e1d1d");
 const container = document.createElement("div");
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
@@ -6,22 +7,15 @@ container.style.height = "800px";
 
 for (let i = 1; i <= 64; i++) {
   const tile = document.createElement("div");
-  if (Math.floor((i - 1) / 8) % 2 === 0) {
-    if (i % 2 === 1) {
-      tile.style.backgroundColor = "black";
-    } else {
-      tile.style.backgroundColor = "red";
-    }
-  } else {
-    if (i % 2 === 1) {
-      tile.style.backgroundColor = "red";
-    } else {
-      tile.style.backgroundColor = "black";
-    }
-  }
   tile.style.width = "12.5%";
   tile.style.height = "12.5%";
   container.append(tile);
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  tile.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
 }
 
 document.body.append(container);
+document.body.append(betterBackground);
